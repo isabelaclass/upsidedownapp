15e69405c91acde8c79e0d8124521f2f8eb92edb
crlnrssa <143560004+crlnrssa@users.noreply.github.com>
2024-04-20 12:05:42 -0300

    upside down commit

--- upsidedownapplication/src/app/upsidedown/page.tsx
+++ upsidedownapplication/src/app/upsidedown/page.tsx
@@ -1,7 +1,37 @@
+import React from "react";
+import './page.css';
+
+type Players = "O" | "X";
 export default function Home() {
+
+    const [turn, setTurn] = React.useState("O");
+    const [winner, setWinner] = React.useState<Players | null>(null);
+    const [tie, setTie] = React.useState<boolean | null>(null);
+    const [marks, setMarks] = React.useState< {[key: string]: Players}>({});
+
+    const getSquares = () => {
+      return new Array(9).fill(true);
+    }
+
+    const play = (index: number) => {
+      setMarks(prev => ({ ...prev, [index]: turn}))
+      setTurn(prev => prev === "O" ? "X" : "O")
+    }
+
     return (
-      <main>
-        <div className="title">Upside down</div>
-      </main>
+      <div className="container">
+        <h1>O ganhou</h1>
+        <h1>Empate</h1>
+        <button>Jogar novamente</button>
+
+        <p>É a vez de X</p>
+        <div className="board">
+        {getSquares().map((_, i) => (
+          <div className="cell" onClick={() => play(i)}>
+          {marks[i]}
+          </div>
+        ))}
+        </div>
+      </div>
     );
   }
